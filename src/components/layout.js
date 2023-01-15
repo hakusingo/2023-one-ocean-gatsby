6747806513c813d87049ee5a57b92884d9d45cb6
haku singo <58970064+hakusingo@users.noreply.github.com>
2023-01-15 15:49:25 +0900

    Revert "init-site"

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,20 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: `var(--size-content)`,
+          padding: `var(--size-gutter)`,
+        }}
+      >
         <main>{children}</main>
-        <footer>
+        <footer
+          style={{
+            marginTop: `var(--space-5)`,
+            fontSize: `var(--font-sm)`,
+          }}
+        >
           © {new Date().getFullYear()} &middot; Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
