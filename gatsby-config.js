2bb568f84be1559c08e80cf510a3be7713bc215f
shingo haku <shingohaku@shingonoMacBook-Pro.local>
2023-01-15 15:46:25 +0900

    init-site

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `沖縄貸切自然体験アドベンチャーツアー｜カヤック・トレッキング・青の洞窟でアドベンチャー体験｜oneocean（ワンオーシャン）`,
+    description: `沖縄貸切自然体験アドベンチャーツアーのワンオーシャン、完全貸切で専門ガイド付き。沖縄本島北部でマングローブカヤック、リバートレッキング、青の洞窟をアドベンチャー体験！貸切だから自分のペースで冒険できる！お子様からご年配まで参加可能なツアーあり。子連れ家族旅行にもおすすめ`,
+    author: `@hakusingo`,
+    siteUrl: `https://one-ocean-toku.com/`,
   },
   plugins: [
     `gatsby-plugin-image`,
